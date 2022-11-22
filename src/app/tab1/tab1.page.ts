af38952ec78856c0af1da7058632ba44b64cb9db
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-11-22 16:50:42 +0200

    Adding error message for name

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -8,14 +9,24 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 })
 export class Tab1Page {
   images: {message: string}[] = [];
-  
-  constructor(private http:HttpClient) {
+
+  constructor(private http:HttpClient, private router:Router) {
     this.http.get('https://dog.ceo/api/breeds/image/random').subscribe((data) => {
       this.images.push({message: data['message']});
     }, (err) => {
         console.log(err);
       })
       
+  }
+
+  goToPredictions(){
+    const name = document.getElementById("name") as HTMLInputElement;
+    const error = document.getElementById("err");
+    if(name.value == ""){
+error.innerText = "Please enter your name!";
+    }else{
+    this.router.navigate(["/tabs/tab2"]);
+  }
   }
 
   
