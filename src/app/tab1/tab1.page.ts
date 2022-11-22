257561f08e9213449bbab9014d9f3d5a216672aa
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-11-22 15:35:43 +0200

    Working on dog api

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  images: {message: string}[] = [];
+  constructor(private http:HttpClient) {
+    this.http.get('https://dog.ceo/api/breeds/image/random').subscribe((data) => {
+      this.images.push({message: data['message']});
+    }, (err) => {
+        console.log(err);
+      })
+      
+  }
 
-  constructor() {}
+  
+
 
 }
+
